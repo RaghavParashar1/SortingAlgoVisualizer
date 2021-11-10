44ed3fec1e8b9e41344d8aabd9623b90ba2a6d61
RaghavParashar1 <rparasharr@gmail.com>
2021-11-10 15:47:31 +0530

    Added array function and buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import sortingalgovisualizer from './sortingalgovisualizer/sortingalgovisualizer'
+import React from 'react';
+import SortingVisualizer from './sortingalgovisualizer/sortingalgovisualizer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <sortingalgovisualizer></sortingalgovisualizer>
+      <SortingVisualizer></SortingVisualizer>
     </div>
   );
 }
--- src/sortingalgovisualizer/sortingalgovisualizer.jsx
+++ src/sortingalgovisualizer/sortingalgovisualizer.jsx
@@ -1,30 +1,64 @@
-import react from "react";
-import './sortingalgovisualizer.css'
+import React from 'react';
+import './sortingalgovisualizer.css';
 
+// Change this value for the number of bars (value) in the array.
+const NUMBER_OF_ARRAY_BARS = 310;
 
-export default class sortingalgovisualizer extends React.Component{
-    constructor(props){
-        super(props);
+// This is the main color of the array bars.
+const PRIMARY_COLOR = 'turquoise';
 
-        this.state = {
-            array:[],
-        };
+export default class SortingVisualizer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      array: [],
+    };
+  }
+
+  componentDidMount() {
+    this.resetArray();
+  }
+
+  resetArray() {
+    const array = [];
+    for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
+      array.push(randomIntFromInterval(5, 730));
     }
-    // When app reload than the array resets itself
-    componentDidMount(){
-        this.resetArray();
-    }
+    this.setState({array});
+  }
 
-    resetArray(){
-        const array = [];
-        for(let i=0;i<=100;i++){
-            array.push(randomIntfromInterval(5, 10000));
-        }
-        this.setState({array});
-    }
 
-    render(){
-        const {array} = this.state;
-        
-    }
+  render() {
+    const {array} = this.state;
+
+    return (
+      <div className="array-container">
+        {array.map((value, idx) => (
+          <div
+            className="array-bar"
+            key={idx}
+            style={{
+              backgroundColor: PRIMARY_COLOR,
+              height: `${value}px`,
+            }}></div>
+        ))}
+        <button onClick={() => this.resetArray()}>Generate New Array</button>
+        <button onClick={() => this.mergeSort()}>Merge Sort</button>
+        <button onClick={() => this.quickSort()}>Quick Sort</button>
+        <button onClick={() => this.heapSort()}>Heap Sort</button>
+        <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+        <button onClick={() => this.testSortingAlgorithms()}>
+          Test Sorting Algorithms (BROKEN)
+        </button>
+      </div>
+    );
+  }
 }
+
+// From https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
